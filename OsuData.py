73e045ffe99585dcd63dc0874cf85d447e873a54
D1scream <sorento_alba@list.ru>
2024-03-16 15:27:46 +0800

    ListOfSquad

--- OsuData.py
+++ OsuData.py
@@ -1,13 +1,13 @@
 from ossapi import *
 import datetime
-import psycopg2
+import sys
+import sqlite3
 client_id = 30992
 client_secret = "Qy1ezYjsN0VdPHla2uPuCwwEQ79E58m5RNplrSqj"
 api = Ossapi(client_id, client_secret)
-today = datetime.datetime
-def GetUserInfo(id):
+def GetUserInfo(id,Key=UserLookupKey.ID):
         try:
-            user = api.user(id, key=UserLookupKey.ID)
+            user = api.user(id, key=Key)
             if(user.is_bot==True):
                   return None
             if(user.statistics.play_time<6000):
@@ -18,39 +18,76 @@ def GetUserInfo(id):
         except Exception as error:
                #print(str(id)+str(error)+" error")
                return None
-def WriteUserToDB(user,id):
-      pass
-me = GetUserInfo(34699410)
-
 GeneralSovSize=40000000
 step = round(GeneralSovSize/100)
-for i in range(0,GeneralSovSize,step):
-        user = GetUserInfo(i)
-        if user:
-            print(GetUserInfo(i))
-            WriteUserToFile(GetUserInfo(i))
 
 
-try:
-    connection = psycopg2.connect(user="postgres",
-                                  password="9132",
-                                  host="localhost",
-                                  port="5432",
-                                  database="OsuDB")
+def executeQuery(query,vars=None):
+    connection = sqlite3.connect('OsuData.db')
     cursor = connection.cursor()
+    try:
+        if(vars is None):
+            cursor.execute(query)
+        else:
+             cursor.execute(query,vars)
+        connection.commit()
+    except Exception as error:
+        print("Ошибка при работе с СУБД", error)
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+def CreateTable():
+    drop_table = "DROP TABLE users"
+    executeQuery(drop_table)
     create_table_query = '''  CREATE TABLE users
-                              (ID INT PRIMARY KEY     NOT NULL,
-                              username           TEXT    NOT NULL,
-                              playtime INT REAL); '''
-    cursor.execute(create_table_query)
-    connection.commit()
+                                (id INTEGER PRIMARY KEY NOT NULL,
+                                username TEXT NOT NULL,
+                                country TEXT,
+                                rank INTEGER,
+                                country_rank INTEGER,
+                                accuracy REAL,
+                                playcount INTEGER,
+                                playtime INTEGER,
+                                playstyle INTEGER,
+                                isactive BOOLEAN); '''
+    executeQuery(create_table_query)
     print("Таблица успешно создана в PostgreSQL")
+def WriteUserToDB(user):
+    if(user is None):
+         return
+    query = "INSERT INTO users (id, username, country, rank, country_rank, accuracy, playcount, playtime, playstyle, isactive) \
+        VALUES (@id, @username, @country, @rank, @country_rank, @accuracy, @playcount, @playtime, @playstyle, @isactive);"
+    executeQuery(query,{
+                 "id":int(user[0]),
+                 "username":user[1],
+                 "country":user[2],
+                 "rank":int(user[3]),
+                 "country_rank":int(user[4]),
+                 "accuracy":float(user[5]),
+                 "playcount":int(user[6]),
+                 "playtime":int(user[7]),
+                 "playstyle":int(user[8]),
+                 "isactive":bool(user[9])}
+                 )
+CreateTable()
 
-except (Exception, Error) as error:
-    print("Ошибка при работе с PostgreSQL", error)
-finally:
-    if connection:
-        cursor.close()
-        connection.close()
-        print("Соединение с PostgreSQL закрыто")
+squad = [
+        GetUserInfo("GodRoPoNiKa",UserLookupKey.USERNAME),
+        GetUserInfo("katalashka boy",UserLookupKey.USERNAME),
+        GetUserInfo("D1scream",UserLookupKey.USERNAME),
+        GetUserInfo("HandsomeMe",UserLookupKey.USERNAME),
+        GetUserInfo("azaz08967565",UserLookupKey.USERNAME),
+        GetUserInfo("Ben_Kir",UserLookupKey.USERNAME),
+        GetUserInfo("skill_issue",UserLookupKey.USERNAME),
+        GetUserInfo("Ahrome",UserLookupKey.USERNAME),
+        GetUserInfo("Ravexi",UserLookupKey.USERNAME),
+        GetUserInfo("mewea",UserLookupKey.USERNAME),
+        GetUserInfo("Shootnik",UserLookupKey.USERNAME),
+        GetUserInfo("YungVenuz",UserLookupKey.USERNAME),
+        GetUserInfo("_kurayami",UserLookupKey.USERNAME),
 
+         ]
+for user in squad:
+     WriteUserToDB(user)
+    

3d88db31917ffcacbec3e486cb9c2e19abea2939
D1scream <sorento_alba@list.ru>
2024-03-17 21:55:20 +0800

    Full Work Get OsuPlayers

--- OsuData.py
+++ OsuData.py
@@ -1,6 +1,7 @@
 from ossapi import *
 import datetime
 import sys
+import random
 import sqlite3
 client_id = 30992
 client_secret = "Qy1ezYjsN0VdPHla2uPuCwwEQ79E58m5RNplrSqj"
@@ -8,45 +9,48 @@ api = Ossapi(client_id, client_secret)
 
 def GetUserInfo(id,Key=UserLookupKey.ID):
         try:
+            user = None
             if(Key==UserLookupKey.ID):
                 user = executeQuery("SELECT * FROM users WHERE id = @id",{"id" : id})
-                try:
-                     l=user[0][3]
-                     
-                     return user[0]
-                except:
-                    pass
-            
+                
             elif(Key==UserLookupKey.USERNAME):
-                user = executeQuery("SELECT * FROM users WHERE username = @id",{"id" : id})
-                try:
-                     l=user[0][3]
-                     return user[0]
-                except:
-                    pass
-            else:
-                 print("fuck")
-                 return None
-            
-            user = api.user(id, key=Key)
+                 user = executeQuery("SELECT * FROM users WHERE username = @id",{"id" : id})
+            #If user Not Empty
+            try:
+                l=user[0][3]
+                return user[0]
+            except:
+                user = api.user(id, key=Key)
             if(user.is_bot==True):
+                  #print("Bot")
                   return None
-            if(user.statistics.play_time<6000):
+            
+            if(user.statistics.play_time<6000 and False):
+                  #print("low time")
                   return None
-            if(not user.is_active):
+            
+            if(not user.is_active and False):
+                 #print("not active")
                  return None
-            info=[str(user.id),user.username,str(user.country.name),str(user.statistics.global_rank),str(user.statistics.country_rank),str(user.statistics.hit_accuracy)\
-            ,str(user.statistics.play_count),str(user.statistics.play_time),str(user.playstyle),str(user.is_active)]
+            if(user.statistics.global_rank>1000000):
+                 #print("low rank ",user.statistics.rank)
+                 return None
+            info=[str(user.id),user.username,
+                  str(user.country.name),
+                  str(user.statistics.global_rank),
+                  str(user.statistics.country_rank),
+                  str(user.statistics.hit_accuracy),
+                  str(user.statistics.play_count),
+                  str(user.statistics.play_time),
+                  str(user.statistics.pp),
+                  str(user.playstyle),
+                  str(user.is_active)]
             print("Added")
             print(info)
             return info
         except Exception as error:
-               print(str(id)+str(error)+" error")
+               #print(str(id)+" "+str(error))
                return None
-GeneralSovSize=40000000
-step = round(GeneralSovSize/100)
-
-
 def executeQuery(query,vars=None):
     connection = sqlite3.connect('OsuData.db')
     cursor = connection.cursor()
@@ -55,25 +59,22 @@ def executeQuery(query,vars=None):
             cursor.execute(query)
         else:
             cursor.execute(query,vars)
-
         list = []
         for i in cursor.fetchall():
              list.append(i)
-        #print(list)
         connection.commit()
         return list
         
     except Exception as error:
          pass
-        #print("Ошибка при работе с СУБД", error)
+        #print("Ошибка при работе с СУБД ", error)
     finally:
         if connection:
             cursor.close()
             connection.close()
-EmptyAns=executeQuery("SELECT * FROM users WHERE id = 0")
 def CreateTable():
     drop_table = "DROP TABLE users"
-    #executeQuery(drop_table)
+    executeQuery(drop_table)
     create_table_query = '''  CREATE TABLE users
                                 (id INTEGER PRIMARY KEY NOT NULL,
                                 username TEXT NOT NULL,
@@ -83,25 +84,24 @@ def CreateTable():
                                 accuracy REAL,
                                 playcount INTEGER,
                                 playtime INTEGER,
+                                pp REAL,
                                 playstyle INTEGER,
                                 isactive BOOLEAN); '''
     executeQuery(create_table_query)
 def WriteUserToDB(user):
-
     if(user is None):
-         
          return
     try:
-         l = int(user[8])
+         l = int(user[9])
     except:
-         user[8]=0
+          user[9]=0
     try:
          l = int(user[3])
     except:
          print(user[1]," No pp")
          return
-    query = "INSERT INTO users (id, username, country, rank, country_rank, accuracy, playcount, playtime, playstyle, isactive) \
-        VALUES (@id, @username, @country, @rank, @country_rank, @accuracy, @playcount, @playtime, @playstyle, @isactive);"
+    query = "INSERT INTO users (id, username, country, rank, country_rank, accuracy, playcount, playtime,pp, playstyle, isactive) \
+        VALUES (@id, @username, @country, @rank, @country_rank, @accuracy, @playcount, @playtime, @pp, @playstyle, @isactive);"
     executeQuery(query,{
                  "id":int(user[0]),
                  "username":user[1],
@@ -111,77 +111,20 @@ def WriteUserToDB(user):
                  "accuracy":float(user[5]),
                  "playcount":int(user[6]),
                  "playtime":int(user[7]),
-                 "playstyle":int(user[8]),
-                 "isactive":bool(user[9])}
+                 "pp":float(user[8]),
+                 "playstyle":int(user[9]),
+                 "isactive":bool(user[10])}
                  )
-    
-    
-CreateTable()
-nicks = [
-        "GodRoPoNiKa",
-        "katalashka boy",
-        "HandsomeMe",
-        "azaz08967565",
-        "Ben_Kir",
-        "skill_issue",
-        "Ahrome",
-        "Ravexi",
-        "mewea",
-        "Shootnik",
-        "YungVenuz",
-        "_kurayami",
-        "katalashka",
-        "Lyeli",
-        "VLADYUSHYA",
-        "Haena-",
-        "Mihlo",
-        "KeS_Play",
-        "AntoN",
-        "BeHot",
-        "pr0xladno",
-        "Yukarle",
-        "silversnax",
-        "Ice Shark",
-        "6adf0X",
-        "Cangrani",
-        "FlyingShark",
-        "boganetdoma",
-        "navkid",
-        "ti2mu2r1",
-        "kokoya",
-        "eruhar",
-        "C_helove_k",
-        "Creppy_",
-        "-RedSky-",
-        "shimaii",
-        "KrestanSXXX",
-        "exp1rian",
-        "sanacura",
-        "ZonnYT",
-        "FedRONI",
-        "Nqo_love",
-        "NeKiTnIkE",
-        "TATAPCTAH",
-        "owoPeef",
-        "MaksatR",
-        "k0_0dama",
-        "tortik122421",
-        "Naabway",
-        "Shui",
-        "_Kaibi_",
-        "Ruslanmasterpro",
-        "Y0kuH",
-        "Reguxo",
-        "gerDen",
-        "N3K4",
-        "M13er",
-        "D1scream",
-        "BaconCat",
-        "RamkaSMEN",
-        "KROKSMAN"
 
-]
-
-for nick in nicks:
-     WriteUserToDB( GetUserInfo(nick,UserLookupKey.USERNAME))
+GeneralSovSize=40000000
+mask = [0]*GeneralSovSize
+j=10
+while(j!=0):
+    ran = random.randint(0,len(mask)-1)
+    if(mask[ran]==0):
+          WriteUserToDB(GetUserInfo(ran)) #Добавить user с рандомным id в базу
+          mask[ran]=1
+          #print(ran)
+    else:
+         print("was") 
     
